2d8ccfa3ab8c2d3c2dcfc9d8e340c3bd660ac5b4
Ekaterina Kiryanova <ekaterina.krashennikova@gmail.com>
2023-04-17 16:55:16 +0500

    feat: pass 1 test vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -17,18 +17,55 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
  * 
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ * ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$(),./|*-&^%', 'ABCDEFGHIJKLMNOPQRSTUVWXYZ
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
+  encrypt(string, key) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if (!string || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    //определяю, насколько текст длиннее, чем ключ
+    let kf = Math.ceil(string.length / key.length);
+    key = key.repeat(kf);
+
+    let codeA = 'A'.charCodeAt(0);
+    let abcCount = 26;
+
+    let result = [];
+
+    for (let i=0; i<string.length; i++) {
+      if (string[i] === ' ') {
+        result.push(string[i]);
+      } else {
+        let letterIdx = string.charCodeAt(i) - codeA;
+        let shift = key.charCodeAt(i) - codeA;
+
+        result.push(
+          String.fromCharCode( codeA + (letterIdx + shift) % abcCount)
+        )
+      }
+    }
+    console.log(result)
+    return result.join('')
+
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
+  decrypt(string, key) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if (!string || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+
+
+
   }
 }
+
+
+
 
 module.exports = {
   VigenereCipheringMachine
