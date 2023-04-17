ab3008ec4173fab1e9b8efc1e0c17a9120a03034
Ekaterina Kiryanova <ekaterina.krashennikova@gmail.com>
2023-04-17 12:28:11 +0500

    feat: pass 4 tests on transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,48 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (!Array.isArray(arr)) {
+    throw new Error ("'arr' parameter must be an instance of the Array!")
+  }
+  if (arr.length === 0) {
+    return [];
+  }
+  for (let i=0; i<arr.length; i++) {
+    //--discard-next исключает следующий элемент массива из преобразованного массива.
+    if (arr[i] === '--discard-next' && arr[i+1] !== undefined ) {
+      let discardNext = arr.slice(0, i) + "," + arr.slice(i + 2);
+      // console.log(discardNext);
+      return discardNext;
+    }
+    //--discard-prev исключает предыдущий элемент массива из преобразованного массива.
+    if (arr[i] === '--discard-prev' && arr[i-1] !== undefined) {
+      let discardPrev = arr.slice(0, i-1) + "," + arr.slice(i+1);
+      // console.log(discardPrev);
+      return discardPrev;
+    }
+    //--double-next дублирует следующий элемент массива в преобразованном массиве.
+    if (arr[i] === '--double-next' && arr[i+1] !== undefined ) {
+      let doubleNext = arr.slice(0, i) + "," + arr[i + 1] + "," + arr.slice(i + 1);
+      // console.log(doubleNext);
+      return doubleNext;
+    }
+    //--double-prev дублирует предыдущий элемент массива в преобразованном массиве.
+    if (arr[i] === '--double-prev' && arr[i-1] !== undefined) {
+      let doublePrev = arr.slice(0, i) + "," + arr[i - 1] + "," + arr.slice(i + 1);
+      // console.log(doublePrev);
+      return doublePrev;
+    }
+  }
 }
+
+
+transform([1, 2, 3, '--discard-next', 4, 5])
+transform([1, 2, 3, '--discard-prev', 4, 5])
+transform([1, 2, 3, '--double-next', 4, 5])
+transform([1, 2, 3, '--double-prev', 4, 5])
 
 module.exports = {
   transform
